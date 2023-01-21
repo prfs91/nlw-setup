4f23ba465bb7dbefa7239501b4b2235b156e8206
= <=>
2023-01-20 20:26:27 -0400

    Salvando no localStorage

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ button.addEventListener(
   add
 ) /* adiciona algo que vai estar ouvindo o evento */
 
+form.addEventListener("change", save)
+
 function add() {
   /* Date é uma função existente na biblioteca da linguagem, toLocaleDateString é outra função retorna a data atual, pt-br é pra ser no estilo de data do Brasil e não americana que é padrão, se estiver vazio fica no estilo mm/dd/yyyy, o slice é uma função que a gente pode recortar, fatiar uma string, no caso queremos apenas o dia e o mês da data*/
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
@@ -23,11 +25,11 @@ function add() {
   alert("Dia adicionado com sucesso ✅")
   nlwSetup.addDay(today)
 }
-// const data = {
-//   run: ["01-20", "01-21", "01-22"],
-//   takePills: ["01-20"],
-//   journal: ["01-25"],
-// }
 
-// nlwSetup.setData(data)
-// nlwSetup.load()
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
